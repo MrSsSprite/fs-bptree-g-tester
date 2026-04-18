9dd8172d296a5a6a26132da158c09dae03f4759c
Archeus Lam <mrsssprite@gmail.com>
2026-04-18 20:34:17 +0800

    test(fflush): draft unity setup

--- test/fflush/src/main.c
+++ test/fflush/src/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include "unity.h"
+
+void setUp(void) { }
+
+void tearDown(void) { }
 
 
 int main(void)
 {
    puts("Test Unit: fflush");
+   UNITY_BEGIN();
+
+   return UNITY_END();
 }
