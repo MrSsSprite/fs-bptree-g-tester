0a799ac72f8bec2c279416ab6e19ed56c339324c
Archeus Lam <mrsssprite@gmail.com>
2026-04-18 20:46:45 +0800

    build(make): include CORE_DIR in INCLUDES

--- config.mk
+++ config.mk
@@ -8,13 +8,15 @@ ROOT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 CC         := gcc
 CFLAGS     := -Wall -Wextra
 UNITY_DIR  := $(ROOT_DIR)/deps/unity
-SRC_DIR    := $(ROOT_DIR)/core/src
+CORE_DIR   := $(ROOT_DIR)/core
+SRC_DIR    := $(CORE_DIR)/src
 TEST_DIR   := $(ROOT_DIR)/test
 OBJ_DIR    := $(ROOT_DIR)/obj
 BIN_DIR    := $(ROOT_DIR)/bin
 RESULT_DIR := $(ROOT_DIR)/result
 
-INCLDUES = $(UNITY_DIR)/src/
+CORE_H_DIRS := $(shell find $(CORE_DIR) -name "*.h" -exec dirname {} + | sort -u)
+INCLDUES = $(UNITY_DIR)/src/ $(CORE_H_DIRS)
 CPPFLAGS = $(addprefix -I, $(INCLDUES))
 
 UNITY_SRC := $(UNITY_DIR)/src/unity.c
