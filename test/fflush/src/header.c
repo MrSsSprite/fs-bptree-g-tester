b986c09b574502c44f4b4ee45b518ed89b47197a
Archeus Lam <mrsssprite@gmail.com>
2026-04-19 13:21:27 +0800

    test(fflush): add load test after bptr init
    After all templates are created. Test each instance and check if each of
    the instances matches the template.

--- test/fflush/src/header.c
+++ test/fflush/src/header.c
@@ -25,6 +25,7 @@ int cmp_u32(const void *lhs, const void *rhs)
 int cmp_u64(const void *lhs, const void *rhs)
 { return (const uint64_t *)lhs - (const uint64_t *)rhs; }
 void _bptr_create(struct bptr_temp *template);
+void _bptr_load_check(struct bptr_temp *template);
 /*-------------------- Private Function Declarations END ---------------------*/
 
 
@@ -36,34 +37,52 @@ struct bptr_temp lite_temps[] =
    { "lite_1024_u64_u64.bptr", 1, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
    { "lite_2048_u64_u64.bptr", 1, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
 };
+size_t lite_temps_sz = sizeof(lite_temps)/sizeof(*lite_temps);
 struct bptr_temp norm_temps[] =
 {
-   { "lite_DEF_u32_u32.bptr", 0, BPTR_NODE_BYTE_DEFAULT, sizeof(uint32_t), sizeof(uint32_t), cmp_u32 },
-   { "lite_128_u64_u64.bptr", 0, 128, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
-   { "lite_1024_u64_u64.bptr", 0, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
-   { "lite_2048_u64_u64.bptr", 0, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
+   { "norm_DEF_u32_u32.bptr", 0, BPTR_NODE_BYTE_DEFAULT, sizeof(uint32_t), sizeof(uint32_t), cmp_u32 },
+   { "norm_128_u64_u64.bptr", 0, 128, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
+   { "norm_1024_u64_u64.bptr", 0, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
+   { "norm_2048_u64_u64.bptr", 0, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
 };
+size_t norm_temps_sz = sizeof(norm_temps)/sizeof(*norm_temps);
 /*-------------------------- Private Variables END ---------------------------*/
 
 
 /*----------------------------- Public Functions -----------------------------*/
 void test_bptr_create(void)
 {
-   puts("\tLite Templates:");
-   for (size_t i = 0; i < sizeof(lite_temps)/sizeof(*lite_temps); i++)
+   struct bptr_temp *temp_matrix[] =
+    { lite_temps, norm_temps, };
+   size_t temp_matrix_sz[] =
+    { lite_temps_sz, norm_temps_sz, };
+   const char *mes[sizeof(temp_matrix)/sizeof(*temp_matrix)] =
+    { "\tLite Templates:", "\tNorm Templates:" };
+
+   // Initialize Tree
+   for (size_t ti = 0; ti < sizeof(temp_matrix)/sizeof(*temp_matrix); ti++)
     {
-      printf("\t\tStarting Test: %s...", lite_temps[i].fnm);
-      _bptr_create(lite_temps + i);
-      puts("Succeeded.");
-      TEST_ASSERT(remove(lite_temps[i].fnm) == 0);
+      struct bptr_temp *temp_it = temp_matrix[ti];
+      puts(mes[ti]);
+      for (size_t i = 0; i < temp_matrix_sz[ti]; i++)
+       {
+         printf("\t\tStarting Test: %s...", temp_it[i].fnm);
+         _bptr_create(temp_it + i);
+         puts("Succeeded.");
+       }
     }
-   puts("\tNorm Templates:");
-   for (size_t i = 0; i < sizeof(norm_temps)/sizeof(*norm_temps); i++)
+
+   // Load and check
+   for (size_t ti = 0; ti < sizeof(temp_matrix)/sizeof(*temp_matrix); ti++)
     {
-      printf("\t\tStarting Test: %s...", norm_temps[i].fnm);
-      _bptr_create(norm_temps + i);
-      puts("Succeeded.");
-      TEST_ASSERT(remove(norm_temps[i].fnm) == 0);
+      struct bptr_temp *temp_it = temp_matrix[ti];
+      puts(mes[ti]);
+      for (size_t i = 0; i < temp_matrix_sz[ti]; i++)
+       {
+         _bptr_load_check(temp_it + i);
+         TEST_ASSERT_MESSAGE(remove(temp_it[i].fnm) == 0,
+                             "file remove failure");
+       }
     }
 }
 /*--------------------------- Public Functions END ---------------------------*/
@@ -77,5 +96,21 @@ void _bptr_create(struct bptr_temp *template)
                                  template->val_sz, template->cmp);
    TEST_ASSERT(bptr);
    TEST_ASSERT(bptr_unload(bptr) == BPTR_E_SUCCESS);
+}
+
+
+void _bptr_load_check(struct bptr_temp *template)
+{
+   struct bptr *bptr = bptr_load(template->fnm);
+
+   TEST_ASSERT_MESSAGE(bptr, "bptr_load failure");
+   TEST_ASSERT_EQUAL(bptr->compare, template->cmp);
+   TEST_ASSERT_EQUAL(bptr->free_list.cnt, 0);
+   TEST_ASSERT_EQUAL(bptr->is_lite, template->is_lite);
+   TEST_ASSERT_EQUAL(bptr->node_size, template->node_sz);
+   TEST_ASSERT_EQUAL(bptr->key_size, template->key_sz);
+   TEST_ASSERT_EQUAL(bptr->value_size, template->val_sz);
+
+   TEST_ASSERT_MESSAGE(bptr_unload(bptr) == 0, "bptr_unload failure");
 }
 /*-------------------------- Private Functions END ---------------------------*/
