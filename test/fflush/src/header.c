6b4b3167dd3986462290fc826fcb2ca76e2fc1e1
Archeus Lam <mrsssprite@gmail.com>
2026-04-19 09:42:10 +0800

    test(fflush): correct TEST_ASSERT to E_SUCCESS
    `test_direct_flust` incorrectly asserted `bptr_unload`. Corrected to
    assert the return to be `BPTR_E_SUCCESS`, which evaluates to 0.

--- test/fflush/src/header.c
+++ test/fflush/src/header.c
@@ -13,5 +13,5 @@ void test_direct_flush(void)
    struct bptr *bptr = bptr_init("direct_flush.bptr", 1, BPTR_NODE_BYTE_DEFAULT,
                                  sizeof(uint32_t), sizeof(uint32_t), cmp_i32);
    TEST_ASSERT(bptr);
-   TEST_ASSERT(bptr_unload(bptr));
+   TEST_ASSERT(bptr_unload(bptr) == BPTR_E_SUCCESS);
 }
