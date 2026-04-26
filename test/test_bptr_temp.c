d476403c0373b8f69fc11bd6e7c982edcb5b00f7
Archeus Lam <mrsssprite@gmail.com>
2026-04-26 13:21:54 +0800

    feat(test_util): add `bptr_temp_tools'
    - Add toolbox functions in test utilities.
    - Inetegrate toolbox functions into bptr templates so that such
      operations can be called by the user regardless of the underlying type
      of the template key & value.

--- test/test_bptr_temp.c
+++ test/test_bptr_temp.c
@@ -1,7 +1,63 @@
 /*----------------------------- Private Includes -----------------------------*/
 #include "test_bptr_temp.h"
 #include "bptr_internal.h"
+#include "bptr_node.h"
+#include <string.h>
 /*--------------------------- Private Includes END ---------------------------*/
+
+
+/*------------------------------ Private Macros ------------------------------*/
+#define _key_ins_i64__generate(T, Suffix) \
+void _key_ins_i64__##Suffix(struct bptr_node *node, int64_t key, size_t idx) \
+{ \
+   if (idx < node->key_count) \
+      memmove((T*)node->keys + idx + 1, (T*)node->keys + idx, \
+              (node->key_count - idx) * sizeof(T)); \
+   ((T*)node->keys)[idx] = key; \
+}
+#define _val_ins_i64__generate(T, Suffix) \
+void _val_ins_i64__##Suffix(struct bptr_node *node, int64_t val, size_t idx) \
+{ \
+   size_t val_cnt = _node_val_cnt(node); \
+   if (idx < val_cnt) \
+      memmove((T*)node->vals + idx + 1, (T*)node->vals + idx, \
+              (val_cnt - idx) * sizeof(T)); \
+   ((T*)node->vals)[idx] = val; \
+}
+
+#define _kv_ins_i64__generate(T, S) \
+   _key_ins_i64__generate(T, S) _val_ins_i64__generate(T, S)
+
+
+#define _key_ers__generate(T, S) \
+void _key_ers__##S(struct bptr_node *node, size_t idx) \
+{ \
+   if (idx < node->key_count) \
+      memmove((T*)node->keys, (T*)node->keys + 1, \
+              (node->key_count - idx - 1) * sizeof(T)); \
+}
+#define _val_ers__generate(T, S) \
+void _val_ers__##S(struct bptr_node *node, size_t idx) \
+{ \
+   size_t val_cnt = _node_val_cnt(node); \
+   if (idx < val_cnt) \
+      memmove((T*)node->vals, (T*)node->vals + 1, \
+              (val_cnt - idx - 1) * sizeof(T)); \
+}
+
+#define _kv_ers__generate(T, S) \
+   _key_ers__generate(T, S) _val_ers__generate(T, S)
+
+
+#define _kv_tools_i64__generate(T, S) \
+   _kv_ins_i64__generate(T, S); \
+   _kv_ers__generate(T, S);
+
+#define _temp_tools_block(Sk, Sv) (struct bptr_temp_tools) \
+{ .node = { _key_ins_i64__##Sk, _val_ins_i64__##Sv, \
+            _key_ers__##Sk, _val_ers__##Sv} }
+#define _temp_tools_block_rpt(S) _temp_tools_block(S, S)
+/*---------------------------- Private Macros END ----------------------------*/
 
 
 /*---------------------- Private Function Declarations -----------------------*/
@@ -11,42 +67,54 @@ int cmp_u64(const void *lhs, const void *rhs)
 { return (const uint64_t *)lhs - (const uint64_t *)rhs; }
 /*-------------------- Private Function Declarations END ---------------------*/
 
+/*---------------------------- Toolbox Functions -----------------------------*/
+_kv_tools_i64__generate(uint32_t, u32);
+_kv_tools_i64__generate(uint64_t, u64);
+/*-------------------------- Toolbox Functions END ---------------------------*/
+
+
+/*---------------------------- Toolbox Variables -----------------------------*/
+struct bptr_temp_tools
+   _tools_u32_u32 = _temp_tools_block_rpt(u32),
+   _tools_u64_u64 = _temp_tools_block_rpt(u64);
+/*-------------------------- Toolbox Variables END ---------------------------*/
+
 
 /*----------------------------- Public Variables -----------------------------*/
 struct bptr_temp lite_temps[] =
 {
-   { "lite_DEF_u32_u32.bptr", 1, BPTR_NODE_BYTE_DEFAULT, sizeof(uint32_t), sizeof(uint32_t), cmp_u32 },
-   { "lite_128_u64_u64.bptr", 1, 128, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
-   { "lite_1024_u64_u64.bptr", 1, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
-   { "lite_2048_u64_u64.bptr", 1, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
+   { "lite_DEF_u32_u32.bptr", 1, BPTR_NODE_BYTE_DEFAULT, sizeof(uint32_t), sizeof(uint32_t), cmp_u32, &_tools_u32_u32 },
+   { "lite_128_u64_u64.bptr", 1, 128, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
+   { "lite_1024_u64_u64.bptr", 1, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
+   { "lite_2048_u64_u64.bptr", 1, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
 };
 size_t lite_temps_sz = sizeof(lite_temps)/sizeof(*lite_temps);
 
 struct bptr_temp norm_temps[] =
 {
-   { "norm_DEF_u32_u32.bptr", 0, BPTR_NODE_BYTE_DEFAULT, sizeof(uint32_t), sizeof(uint32_t), cmp_u32 },
-   { "norm_128_u64_u64.bptr", 0, 128, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
-   { "norm_1024_u64_u64.bptr", 0, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
-   { "norm_2048_u64_u64.bptr", 0, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
+   { "norm_DEF_u32_u32.bptr", 0, BPTR_NODE_BYTE_DEFAULT, sizeof(uint32_t), sizeof(uint32_t), cmp_u32, &_tools_u32_u32 },
+   { "norm_128_u64_u64.bptr", 0, 128, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
+   { "norm_1024_u64_u64.bptr", 0, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
+   { "norm_2048_u64_u64.bptr", 0, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
 };
 size_t norm_temps_sz = sizeof(norm_temps)/sizeof(*norm_temps);
 
 
 struct bptr_temp lite_temps_iu[] =
 {
-   { "lite_DEF_u32_u32.bptr", 1, BPTR_NODE_BYTE_DEFAULT, sizeof(uint32_t), sizeof(uint32_t), cmp_u32 },
-   { "lite_128_u64_u64.bptr", 1, 128, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
-   { "lite_1024_u64_u64.bptr", 1, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
-   { "lite_2048_u64_u64.bptr", 1, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
+   { "lite_DEF_u32_u32.bptr", 1, BPTR_NODE_BYTE_DEFAULT, sizeof(uint32_t), sizeof(uint32_t), cmp_u32, &_tools_u32_u32 },
+   { "lite_128_u64_u64.bptr", 1, 128, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
+   { "lite_1024_u64_u64.bptr", 1, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
+   { "lite_2048_u64_u64.bptr", 1, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
 };
 size_t lite_temps_iu_sz = sizeof(lite_temps_iu)/sizeof(*lite_temps_iu);
 
 struct bptr_temp norm_temps_iu[] =
 {
-   { "norm_DEF_u32_u32.bptr", 0, BPTR_NODE_BYTE_DEFAULT, sizeof(uint32_t), sizeof(uint32_t), cmp_u32 },
-   { "norm_128_u64_u64.bptr", 0, 128, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
-   { "norm_1024_u64_u64.bptr", 0, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
-   { "norm_2048_u64_u64.bptr", 0, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64 },
+   { "norm_DEF_u32_u32.bptr", 0, BPTR_NODE_BYTE_DEFAULT, sizeof(uint32_t), sizeof(uint32_t), cmp_u32, &_tools_u32_u32 },
+   { "norm_128_u64_u64.bptr", 0, 128, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
+   { "norm_1024_u64_u64.bptr", 0, 1024, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
+   { "norm_2048_u64_u64.bptr", 0, 2048, sizeof(uint64_t), sizeof(uint64_t), cmp_u64, &_tools_u64_u64 },
 };
 size_t norm_temps_iu_sz = sizeof(norm_temps_iu)/sizeof(*norm_temps_iu);
 /*--------------------------- Public Variables END ---------------------------*/
--- test/test_bptr_temp.h
+++ test/test_bptr_temp.h
@@ -4,6 +4,7 @@
 /*----------------------------- Public Includes ------------------------------*/
 #include <stdint.h>
 #include <stddef.h>
+#include "bptr_node.h"
 /*--------------------------- Public Includes END ----------------------------*/
 
 /*------------------------------ Public Structs ------------------------------*/
@@ -14,6 +15,18 @@ struct bptr_temp
    uint32_t node_sz;
    uint16_t key_sz, val_sz;
    int (*cmp)(const void *, const void *);
+   struct bptr_temp_tools *tools;
+};
+
+struct bptr_temp_tools
+{
+   struct
+    {
+      void (*key_ins_i64)(struct bptr_node*, int64_t, size_t);
+      void (*val_ins_i64)(struct bptr_node*, int64_t, size_t);
+      void (*key_ers)(struct bptr_node*, size_t);
+      void (*val_ers)(struct bptr_node*, size_t);
+    } node;
 };
 /*---------------------------- Public Structs END ----------------------------*/
 
@@ -28,5 +41,26 @@ extern size_t lite_temps_iu_sz;
 extern struct bptr_temp norm_temps_iu[];
 extern size_t norm_temps_iu_sz;
 /*--------------------- Public Variable Declarations END ---------------------*/
+
+
+/*----------------------------- Public Functions -----------------------------*/
+static inline
+void _bptr_kv_ins_i64(struct bptr_node *node, struct bptr_temp_tools *tools,
+                      int64_t key, int64_t val, size_t idx)
+{
+   tools->node.key_ins_i64(node, key, idx);
+   tools->node.val_ins_i64(node, val, node->is_leaf ? idx : idx + 1);
+   node->key_count++;
+}
+
+static inline
+void _bptr_kv_ers(struct bptr_node *node, struct bptr_temp_tools *tools,
+                  size_t idx)
+{
+   tools->node.key_ers(node, idx);
+   tools->node.val_ers(node, idx);
+   node->key_count--;
+}
+/*--------------------------- Public Functions END ---------------------------*/
 
 #endif
