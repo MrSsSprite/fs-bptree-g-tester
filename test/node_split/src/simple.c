86752370dfaa687dab3563a3b150589d60afc147
Archeus Lam <mrsssprite@gmail.com>
2026-05-02 22:00:17 +0800

    fix(n/sp/simp): correct assertion on node_unload
    `bptr_node_unload' returns 0 on success. Asserting the return value
    directly (i.e., assert true) give the opposite result.

--- test/node_split/src/simple.c
+++ test/node_split/src/simple.c
@@ -75,7 +75,7 @@ void test_simp_split_end(struct bptr_temp *temp)
    TEST_ASSERT_BITS(0x3, 0x1, node->flags);
    TEST_ASSERT_EQUAL(1, node->key_count);
 
-   TEST_ASSERT(bptr_node_unload(bptr, node));
+   TEST_ASSERT_EQUAL(0, bptr_node_unload(bptr, node));
    /*------------------- Check Correctness after Split END -------------------*/
 
    TEST_ASSERT_MESSAGE(bptr_unload(bptr) == 0,
