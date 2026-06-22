3eb0c35af988c0a59df53d6cf26b7702d4f11747
Archeus Lam <mrsssprite@gmail.com>
2026-06-22 12:39:03 +0800

    feat(split): add _bptr_full_brch_create util fn
    Implement a `_bptr_full_brch_create' utility function that creates a
    B+Tree with an internal node that is full.

--- test/node_split/src/brch_sp.c
+++ test/node_split/src/brch_sp.c
@@ -1,12 +1,19 @@
 /*----------------------------- Private Includes -----------------------------*/
 #include "brch_sp.h"
 #include <stdio.h>
+#include "bptree.h"
 #include "unity.h"
 #include "test_bptr_temp.h"
 #include "test_bptr_setup.h"
 #include "bptr_node.h"
 #include "bptr_static.h"
 /*--------------------------- Private Includes END ---------------------------*/
+
+
+/*---------------------- Private Function Declarations -----------------------*/
+void _bptr_full_brch_create(struct bptr_temp *temp);
+void test_sing_brch_split(struct bptr_temp *temp);
+/*-------------------- Private Function Declarations END ---------------------*/
 
 
 /*-------------------------------- Test Units --------------------------------*/
@@ -22,6 +29,7 @@ void test_brch_split(void)
       for (size_t tp_it = 0, tp_mx = test_sz_matrix[m_it];
            tp_it < tp_mx; tp_it++)
        {
+         test_sing_brch_split(test_matrix[m_it] + tp_it);
        }
     }
 }
@@ -33,32 +41,60 @@ void test_brch_split(void)
 // and split. The spawned leaf will be the last node.
 void test_sing_brch_split(struct bptr_temp *temp)
 {
+}
+/*---------------------------- Test Processes END ----------------------------*/
+
+
+/*---------------------------- Private Utilities -----------------------------*/
+void _bptr_full_brch_create(struct bptr_temp *temp)
+{
    struct bptr *bptr = _bptr_create(temp);
-   struct bptr_node *node;
+   struct bptr_node *node, *par_n;
    int64_t i = 0;
 
    TEST_ASSERT_MESSAGE(bptr, "failed at _bptr_create");
-
    // Fill up the internal node at level==1
+   // Since Redistribution is not available yet, manually create all the nodes.
    node = bptr_node_new(bptr, 0);
-   for (uint32_t elem_i = 0, elem_mx = bptr->node_bound.leaf.up - 1;
-        elem_i < elem_mx; elem_i++)
-    {
-      _bptr_kv_ins_i64(node, temp->tools, i, i + 1, i);
-      i++;
-    }
-   bptr->record_cnt = node->key_count;
+   TEST_ASSERT_NOT_NULL_MESSAGE(node, "bptr_node_new failure");
+   node->prev = 0;
+   for (uint32_t leaf_i = 0, leaf_mx = bptr->node_bound.leaf.up - 1;
+        leaf_i < leaf_mx; leaf_i++, i++)
+      _bptr_kv_ins_i64(node, temp->tools, i, i * 2, leaf_i);
+   bptr->record_cnt += node->key_count;
    bptr->node_cnt++;
-   TEST_ASSERT_MESSAGE(
-      bptr_node_split(bptr, node,
-                      temp->tools->node.key_wrapper_i64(i),
-                      temp->tools->node.val_wrapper_i64(i + 1)),
-      "Failed at Split");
-   i++;
-   bptr_node_unload(bptr, node);
-   for (uint32_t leaf_i = 0, leaf_mx = bptr->node_bound.brch.up - 1;
-        leaf_i < leaf_mx; leaf_i++)
+
+   par_n = bptr_node_new(bptr, 0);
+   TEST_ASSERT_NOT_NULL_MESSAGE(par_n, "bptr_node_new failure");
+   par_n->prev = par_n->next = 0;
+   node->parent = par_n->node_idx;
+   temp->tools->node.val_ins_i64(par_n, node->node_idx, 0);
+   bptr->node_cnt++;
+
+   for (uint32_t brch_i = 0, brch_mx = bptr->node_bound.brch.up - 1;
+        brch_i < brch_mx; brch_i++)
     {
+      struct bptr_node *next_n = bptr_node_new(bptr, par_n->node_idx);
+      TEST_ASSERT_NOT_NULL_MESSAGE(next_n, "bptr_node_new failure");
+      node->next = next_n->node_idx;
+      next_n->prev = node->node_idx;
+      bptr_node_unload(bptr, node);
+      node = next_n;
+
+      for (uint32_t leaf_i = 0, leaf_mx = bptr->node_bound.leaf.up - 1;
+           leaf_i < leaf_mx; leaf_i++, i++)
+         _bptr_kv_ins_i64(node, temp->tools, i, i * 2, leaf_i);
+      bptr->record_cnt += node->key_count;
+      bptr->node_cnt++;
+
+      _bptr_kv_ins_i64(par_n, temp->tools,
+                       temp->tools->node.cast_i64(node->keys),
+                       node->node_idx, brch_i);
     }
+   node->next = 0;
+   bptr_node_unload(bptr, node);
+   bptr_node_unload(bptr, par_n);
+
+   TEST_ASSERT_EQUAL_MESSAGE(0, bptr_unload(bptr), "bptr_unload failure");
 }
-/*---------------------------- Test Processes END ----------------------------*/
+/*-------------------------- Private Utilities END ---------------------------*/
