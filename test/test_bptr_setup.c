3de7715c5c0e356509184c27f119495d445147a5
Archeus Lam <mrsssprite@gmail.com>
2026-06-24 16:47:53 +0800

    feat(temp): subdir support for _bptr_create
    Add _bptr_path_subdir and _bptr_create_subdir helpers to allow tests
    to create bptree files under bptr_files/<subdir>/ paths, supporting
    test scenarios that require isolated file directories.

--- test/test_bptr_setup.c
+++ test/test_bptr_setup.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <string.h>
 #include "unity.h"
 #include "bptree.h"
 #include "bptr_internal.h"
@@ -16,10 +17,51 @@ void _bptr_path(char *dest, size_t sz, const char *filename)
    }
 }
 
+
+void _bptr_path_subdir
+ (char *dest, size_t sz, const char *filename, const char *subdir)
+{
+   char subpath[256], *it;
+
+   if (mkdir("bptr_files", 0755) != 0 && errno != EEXIST)
+      TEST_FAIL_MESSAGE("Failed to create bptr_files directory");
+   it = stpcpy(subpath, "bptr_files/");
+   for (; it < subpath + sizeof(subpath) - 2 && *subdir; it++, subdir++)
+    {
+      if (*subdir == '/' && mkdir(subpath, 0755) != 0 && errno != EEXIST)
+         TEST_FAIL_MESSAGE("Failed to create sub-directory");
+      *it = *subdir;
+    }
+   if (*subdir != '/')
+    {
+      if (mkdir(subpath, 0755) != 0 && errno != EEXIST)
+         TEST_FAIL_MESSAGE("Failed to create sub-directory");
+      *it++ = '/';
+    }
+   *it = '\0';
+   TEST_ASSERT_LESS_THAN_size_t_MESSAGE(
+      sz, strlen(subpath) + strlen(filename) + 1,
+      "_bptr_path_subdir: path too long");
+   strcpy(stpcpy(dest, subpath), filename);
+}
+
 struct bptr *_bptr_create(struct bptr_temp *template)
 {
    char path[256];
    _bptr_path(path, sizeof(path), template->fnm);
+   struct bptr *bptr = bptr_init(path, template->is_lite,
+                                 template->node_sz, template->key_sz,
+                                 template->val_sz, template->cache_cap,
+                                 template->cmp);
+   TEST_ASSERT_MESSAGE(bptr, "`_bptr_create': `bptr_init' returned NULL");
+   return bptr;
+}
+
+
+struct bptr *_bptr_create_subdir(struct bptr_temp *template, const char *subdir)
+{
+   char path[256];
+   _bptr_path_subdir(path, sizeof(path), template->fnm, subdir);
    struct bptr *bptr = bptr_init(path, template->is_lite,
                                  template->node_sz, template->key_sz,
                                  template->val_sz, template->cache_cap,
--- test/test_bptr_setup.h
+++ test/test_bptr_setup.h
@@ -8,7 +8,11 @@
 
 /*----------------------- Public Function Declarations -----------------------*/
 void _bptr_path(char *dest, size_t sz, const char *filename);
+void _bptr_path_subdir
+ (char *dest, size_t sz, const char *filename, const char *subdir);
 struct bptr *_bptr_create(struct bptr_temp *template);
+struct bptr *_bptr_create_subdir
+ (struct bptr_temp *template, const char *subdir);
 /*--------------------- Public Function Declarations END ---------------------*/
 
 #endif
